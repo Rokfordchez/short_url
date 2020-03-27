2f50346f89c79b8e8c8db42208a2cf65b5e7d3c7
rokfordchez <ilya.tleukenov@gmail.com>
2020-03-28 00:14:31 +0700

    add ajax encode post - not work

--- surl/views.py
+++ surl/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Surl, encode, decode
+from urllib.parse import urlparse
 
-# Create your views here.
+def index(request):
+    if request.method == 'POST':
+        if urlparse(request.POST['url']).scheme == '':
+            url = 'http://' + request.POST['url']
+        else:
+            url = request.POST['url']
+        try:
+            id = Surl.objects.get(url=url).id
+            return render(request, "index.html", {"short_url": 'http://localhost:8000/'+encode(id)})
+        except Surl.DoesNotExist:
+            surl = Surl(url=url)
+            surl.save()
+            return render(request, "index.html", {"short_url": 'http://localhost:8000/'+encode(surl.id)})
+    else:
+        print(request.method)
+        print('blya')
+        return render(request, "index.html")
